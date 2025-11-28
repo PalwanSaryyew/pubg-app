869cfa7a756a80061c716326e6810e75b41b276f
Emeki <palwansary1@gmail.com>
2025-11-28 13:32:59 +0500

    Z-Nex

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
    return (
       <header className="mx-auto flex items-center justify-center py-11 font-bold fixed w-full z-10">
          <h1 className="bg-card/50 backdrop-blur-xl px-3 py-1 rounded-full">
-            PUBG Bazar
+            Z-Nex
          </h1>
          
       </header>
